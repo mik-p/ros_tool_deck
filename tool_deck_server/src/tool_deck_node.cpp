3c633a3f2625a7d506cf6cb68fc7aaa6b78f4114
Michael Pritchard <45908907+mik-p@users.noreply.github.com>
2022-06-03 11:58:45 +1000

    Update tool_deck_node.cpp

--- tool_deck_server/src/tool_deck_node.cpp
+++ tool_deck_server/src/tool_deck_node.cpp
@@ -1,1 +1,17 @@
 
+#include <ros/ros.h>
+#include <tool_deck_server/tool_deck.h>
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "tool_deck_server");
+
+  ros::NodeHandle nh;
+  ros::NodeHandle pnh("~");
+
+  tool_deck_server::ToolDeckServer tdserv(nh, pnh);
+
+  ros::spin();
+
+  return 0;
+}
